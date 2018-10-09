d7daa0be0f52a34d006108fa5bdaedfd00175556
JellyCai <jellycai1995@gmail.com>
2018-10-09 15:06:17 +0800

    tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myfirstflutterapp/platform/platform_widet.dart';
+import 'package:myfirstflutterapp/tabbar/tabbar_widget.dart';
 import 'package:myfirstflutterapp/widget/PageOneWidget.dart';
 
 void main() => runApp(new MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Welcome to Flutter',
       theme: new ThemeData(primaryColor: Colors.blue),
-      home: PlatformWidget(),
+      home: TabBarWidget(),
     );
   }
 }
